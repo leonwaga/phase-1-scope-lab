4350863cfa3514d37eb7434d5c015407fe655d79
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-07-16 19:14:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = '';
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase()
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+
+ function overwriteBestCustomer(newFavorite);
+          bestCustomer = newFavorite;
+
+function changeLeastFavoriteCustomer()
+  leastFavoriteCustomer = 'me';
+}
